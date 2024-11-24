0bc9e0999b5fd544c7c814fabc386d6fce5a771f
Valerii Rudyka <val.rudika@gmail.com>
2024-11-24 20:39:22 +0200

    Initial files

---
+++ main.py
@@ -0,0 +1,14 @@
+from view.main.ui_main import MainWindow
+from controller import Controller
+from PySide6.QtWidgets import QApplication
+import sys
+
+
+app = QApplication()
+window = MainWindow()
+controller = Controller(window)
+window.show()
+
+sys.exit(app.exec())
+
+
