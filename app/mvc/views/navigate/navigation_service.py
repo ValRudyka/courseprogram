d865d45ff89e49352ff03e0eaf2d0875a763ba43
Valerii Rudyka <val.rudika@gmail.com>
2025-03-10 00:55:47 +0200

    update navigation

--- app/mvc/views/navigate/navigation_service.py
+++ app/mvc/views/navigate/navigation_service.py
@@ -1,5 +1,6 @@
 from PySide6.QtCore import QObject
 from PySide6.QtWidgets import QMainWindow
+from mvc.views.mainwindow import MainWindow
 
 class NavigationService(QObject):
     def __init__(self):
@@ -20,3 +21,9 @@ class NavigationService(QObject):
 
         self.views[view_name].show()
         self.curr_view = view_name
+
+    def show_main_window(self) -> None:
+        if not self.main_window or not self.main_window.isVisible():
+           self.main_window = MainWindow()
+
+        self.main_window.show()
