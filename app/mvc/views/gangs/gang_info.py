f3569cdb2ebc75f3a5517575fa2852acf5fb8caa
Valerii Rudyka <val.rudika@gmail.com>
2025-03-18 00:59:30 +0200

    update views

---
+++ app/mvc/views/gangs/gang_info.py
@@ -0,0 +1,9 @@
+from PySide6.QtWidgets import QMainWindow
+from .gang_info import Ui_MainWindow
+
+class CriminalsView(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
