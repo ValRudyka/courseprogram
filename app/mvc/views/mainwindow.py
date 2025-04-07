711ee67b4b06d3ae81d191f7a56199868e467e64
Valerii Rudyka <val.rudika@gmail.com>
2025-04-07 20:14:40 +0300

    add timer in main form

--- app/mvc/views/mainwindow.py
+++ app/mvc/views/mainwindow.py
@@ -1,5 +1,5 @@
 from PySide6.QtWidgets import QMainWindow
-from PySide6.QtCore import Signal
+from PySide6.QtCore import Signal, QTimer, QDateTime, QLocale
 from .main_source import Ui_MainWindow
 
 class MainWindow(QMainWindow):
@@ -14,8 +14,36 @@ class MainWindow(QMainWindow):
 
         self.ui.pushButton_2.clicked.connect(self._on_criminals_action)
         self.ui.pushButton_3.clicked.connect(self._on_groups_action)
+        self.ui.pushButton_4.clicked.connect(self._on_archive_action)
         
-
+        self.setup_time_display()
+    
+    def setup_time_display(self):
+        """Set up the timer to update the date and time display."""
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_time)
+        self.timer.start(1000)
+        
+        self.ukrainian_locale = QLocale(QLocale.Ukrainian, QLocale.Ukraine)
+        
+        self.update_time()
+    
+    def update_time(self):
+        """Update the time display with current date and time in Ukrainian format."""
+        now = QDateTime.currentDateTime()
+        
+        day_name = self.ukrainian_locale.toString(now, "dddd")
+        day_number = now.toString("d")
+        month_name = self.ukrainian_locale.toString(now, "MMMM")
+        
+        date_text = f"{day_name}, {day_number} {month_name}"
+        
+        time_text = now.toString("HH:mm:ss")
+        
+        full_text = f"{date_text}\n{time_text}"
+        
+        self.ui.label_2.setText(full_text)
+        
     def _on_criminals_action(self):
         self.open_criminals_requested.emit()
 
